8c4dc221fcc2d56234c2b3f51e3c12e9b23696a4
Carter Broom <carterdbroom@gmail.com>
2024-08-18 21:39:27 -0400

    Mapping out response statements.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,28 @@ use::std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
+    
+    
+    // Response
+    println!("The river flow on the {river} at {section} is {flow}cms today🌊");
+    println!("Do you want to look another river up? [y/n]");
+    //Case of y
+    println!("Enter the river and the station");
+
+    // Case of n
+    println!("Have fun on the river!");
+
+    println!("The river is {river} and the section is {section}");
+}
+
+fn parse_config(args: &[String]) -> Config {
     let river = &args[1];
     let section = &args[2];
+    Config {river, section}
+}
 
-    println!("The river is {river} and the section is {section}")
+struct Config {
+    river: String,
+    section: String
+
 }
