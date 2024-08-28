6e770c8cf90c0a15a4edb3b773c6cc68565d2de6
Carter Broom <carterdbroom@gmail.com>
2024-08-28 10:09:10 -0400

    Program runs now, worked on game logic.

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ struct FlowCall {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct FlowData {
-    #[serde(rename(deserialize = "endDate"))]
+    #[serde(rename(deserialize = "startDate"))]
     start_date: String,
     #[serde(rename(deserialize = "endDate"))]
     end_date: String,
@@ -50,48 +50,82 @@ const KEY: &str = "-O5A-mmDjkK19KriaFk0";
 fn main() {
     
     let client: Client = Client::new();
-    //println!("{}", get_river_flow(&client, &id).as_str());
-
-    println!("Enter the name of the river or creek you would like to get data for: ");  
-
-    let mut input = String::new();
-
-    io::stdin().read_line(&mut input).unwrap();
-
+    let mut program_running: bool = true;
     let river_list: Vec<River> = deserialize_river_list(get_river_list(&client).as_str());
+    //println!("{}", get_river_flow(&client, &id).as_str());
+    while program_running {
+        println!("Enter the name of the river or creek you would like to get data for: ");  
 
-    let mut matches = Vec::new();
-    let mut counter: u32 = 0;
-    println!("Here are the results:");
-    for river in &river_list {
-        if river.name.contains(input.to_uppercase().trim()) {
-            counter += 1;
-            matches.push((&river.name, &river.id));
-            println!("[{}] {}", counter, &river.name);
-        }
-    }
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
 
-    if matches.len() > 0 {
-        
-        let mut number = String::new();
-        
-        println!("Enter the number next to the station you would like to choose:");
-        
-        io::stdin().read_line(&mut number).unwrap();
-        
-        match number.parse::<usize>() {
-            Ok(n) => {
-                let chosen_id: &String = &matches[n-1].1;
-            },
-            Err(_) => {
-                println!("Enter a valid number: ")
+        let mut matches = Vec::new();
+        let mut counter: u32 = 0;
+        println!("Here are the results:\n");
+        for river in &river_list {
+            if river.name.contains(input.to_uppercase().trim()) {
+                counter += 1;
+                matches.push((&river.name, &river.id));
+                println!("[{}] {}", counter, &river.name);
             }
         }
-    }
-    else {
-        println!("Unable to find such a river in the database.")
-    }
-    
+
+        if matches.len() > 0 {
+            
+            loop {
+            
+                println!("\nEnter the number next to the station you would like to choose:");
+                
+                let mut number = String::new();
+                
+                io::stdin().read_line(&mut number).unwrap();
+                
+                match number.trim().parse::<usize>() {
+                    Ok(n) => {
+                        if n <= matches.len() && n > 0 {
+                            let chosen_river_id: &String = &matches[n-1].1;
+                            let flow_data_list = deserialize_river_flow(get_river_flow(&client, &chosen_river_id).as_str());
+                            let last_element: usize = flow_data_list.len() - 1;
+                            let latest_river_flow = &flow_data_list[last_element].value;
+                            let date = &flow_data_list[last_element].date;
+                            println!("Displaying Data:\n");
+                            println!("Station Name: {} 🚧", &matches[n-1].0);
+                            println!("Date: {} 📅", date);
+                            println!("River Flow: {} cubic metres per second 🌊\n", &latest_river_flow);
+                            println!("Would you like to get data for another river? [y/n]");
+
+                            let mut answer = String::new();
+                            io::stdin().read_line(&mut answer).unwrap();
+                            if answer.trim().eq_ignore_ascii_case("n") {
+                                program_running = false;
+                            }
+                            break;
+                        }
+                        else {
+                            println!("Your number was either too big or too small!");
+                        }
+                        
+                    },
+                    Err(_) => {
+                        println!("You entered a non-valid number!")
+                    }
+                }    
+            }
+        }
+        else {
+            println!("Unable to find such a river in the database.");
+            println!("Would you like to search for another river? [y/n]");
+
+            let mut answer = String::new();
+
+            io::stdin().read_line(&mut answer).unwrap();
+
+            if answer.trim().eq_ignore_ascii_case("n") {
+                program_running = false;
+            }
+        }
+    }    
+    println!("Have fun on the river! 🤙")
 }
 
 fn get_river_list (client: &Client) -> String {
@@ -149,10 +183,10 @@ fn get_river_flow (client: &Client, id: &String) -> String {
 }
 
 fn deserialize_river_flow (json_string: &str) -> Vec<Flow>{
-    let river_flow_list = from_str::<FlowData>(json_string);
+    let river_flow_list = from_str::<FlowCall>(json_string);
     match river_flow_list {
         Ok(flow_list) => {
-            flow_list.history
+            flow_list.message.history
         }
         Err(e) => {
             println!("{:?}", e);
